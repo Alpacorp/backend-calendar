a6aa93e6636d8ef6498c4cef2db564e8da63f99f
Alejandro Palacios Arevalo <A2P7A3A0@fs.net>
2022-09-15 17:06:18 -0500

    initial commit to start

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+require("dotenv").config();
+
+console.log(process.env.PORT);
+
+// Create express server
+const app = express();
+
+// Public Directory
+app.use(express.static("public"));
+
+//Rouites
+app.use("/api/auth", require("./routes/auth"));
+// TODO: CRUD: events
+
+// Listen petitions
+app.listen(4000, () => {
+  console.log(`Server on port ${4000}`);
+});
---
+++ routes/auth.js
@@ -0,0 +1,28 @@
+// Rutas de usuarios / Auth
+// host + /api/auth
+
+const { Router } = require("express");
+const router = Router();
+
+router.post("/new", (req, res) => {
+  res.json({
+    ok: true,
+    msg: "registro",
+  });
+});
+
+router.post("/", (req, res) => {
+  res.json({
+    ok: true,
+    msg: "login",
+  });
+});
+
+router.get("/renew", (req, res) => {
+  res.json({
+    ok: true,
+    msg: "renew",
+  });
+});
+
+module.exports = router;
